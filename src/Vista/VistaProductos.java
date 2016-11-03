dc9d5bafeb2590ace1f08fcf00ea56b461e8e258
ESTACIONPC <ESTACIONPC@172.18.16.159>
2016-11-02 19:45:22 -0500

    .

--- src/Vista/VistaProductos.java
+++ src/Vista/VistaProductos.java
@@ -14,7 +14,7 @@ public class VistaProductos extends javax.swing.JFrame {
     /**
      * Creates new form VistaProductos
      */
-    public VistaProductos() {
+    public VistaProductos()  {
         initComponents();
     }
 
